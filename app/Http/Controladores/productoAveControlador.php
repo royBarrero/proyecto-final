7e3d5338f83e10cc0b713656efa060e2d635a192
Huari29 <cristoteam29@gmail.com>
2025-10-23 20:39:28 -0400

    bitacora hora actualizada

--- app/Http/Controladores/productoAveControlador.php
+++ app/Http/Controladores/productoAveControlador.php
@@ -13,8 +13,8 @@ class ProductoAveControlador extends Controlador
     {
         $aves = ProductoAve::with(['categoria', 'detalleAve'])->orderBy('id', 'asc')->get();
         return response()->view('productoAves.principal', compact('aves'))->header('Cache-Control', 'no-cache, no-store, must-revalidate')
-    ->header('Pragma', 'no-cache')
-    ->header('Expires', '0');
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', '0');
     }
 
     public function create()
