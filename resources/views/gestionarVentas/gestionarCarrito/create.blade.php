9d562aa55e843cc5441c785187444af25914e01b
Huari29 <cristoteam29@gmail.com>
2025-11-04 11:15:02 -0400

    ventas->index->linea27

--- resources/views/gestionarVentas/gestionarCarrito/create.blade.php
+++ resources/views/gestionarVentas/gestionarCarrito/create.blade.php
@@ -17,7 +17,17 @@
         </div>
 
         {{-- Enviar el ID del usuario logueado como vendedor automáticamente --}}
-        <input type="hidden" name="id_vendedor" value="{{ Auth::user()->id }}">
+        {{--<input type="hidden" name="id_vendedor" value="{{ Auth::user()->id }}">--}}
+        <div class="form-group">
+            <label>Cliente</label>
+            <select name="id_cliente" required>
+                @foreach($clientes as $c)
+                    <option value="{{ $c->id }}" {{ $venta->id_cliente == $c->id ? 'selected' : '' }}>
+                        {{ $c->idusuarios }}
+                    </option>
+                @endforeach
+            </select>
+        </div>
 
         <div class="form-group">
             <label>Método de Pago</label>
